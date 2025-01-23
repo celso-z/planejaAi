531e31556cdaa001b7c3e391b05183c1b417c54f
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-01-23 09:45:06 -0300

    Primeiro commit, repositório criado

---
+++ src/test/java/com/UFJF/planejaai/PlanejaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UFJF.planejaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
