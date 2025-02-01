e1ca4bc146a5699935e0d90612af021db7ac6107
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-01 16:32:43 -0300

    Implementado teste na classe Usuario

---
+++ src/test/java/com/UFJF/planejaai/domain/UsuarioTests.java
@@ -0,0 +1,38 @@
+package com.UFJF.planejaai.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+import java.util.Set;
+
+
+@SpringBootTest
+public class UsuarioTests {
+	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	
+	@Test
+	void constraintsTest() {
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<Usuario>> violations;
+		Usuario example = new Participante();
+		example.setNome("");
+		example.setEmail("user@system.com");
+		
+		violations = validator.validate(example);
+		Assertions.assertTrue(violations.size() == 1, "Validação de campo nome vazio incorreta!");
+		violations.clear();
+		example.setNome("user");
+		example.setEmail("");
+		
+		violations = validator.validate(example);
+		Assertions.assertTrue(violations.size() == 1, "Validação de campo email vazio incorreta!");
+		
+		
+	}
+}
