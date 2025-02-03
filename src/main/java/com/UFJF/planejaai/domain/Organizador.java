630e2aa3d8cbe9d5a6ad3fe259700fcb6688f52c
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-03 17:23:22 -0300

    Implementação básica classe organizador

--- src/main/java/com/UFJF/planejaai/domain/Organizador.java
+++ src/main/java/com/UFJF/planejaai/domain/Organizador.java
@@ -6,5 +6,8 @@ import jakarta.persistence.Entity;
 @Entity
 @DiscriminatorValue("ORGANIZADOR")
 public class Organizador extends Usuario {
-
+	public Organizador() {
+		super();
+		this.setTipoUsuario(TipoUsuario.ORGANIZADOR);
+	}
 }
