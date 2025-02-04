b54ee566bfd34bcc0b1b8f08aeab27c4ea4cf202
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-04 13:02:57 -0300

    Implementação básica classe repositório de palestrantes utilizando JPA

---
+++ src/main/java/com/UFJF/planejaai/repositories/PalestranteRepository.java
@@ -0,0 +1,7 @@
+package com.UFJF.planejaai.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.UFJF.planejaai.domain.Palestrante;
+
+public interface PalestranteRepository extends CrudRepository<Palestrante, Long> {
+}
