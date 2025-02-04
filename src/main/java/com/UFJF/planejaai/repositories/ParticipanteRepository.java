9dec8a7cdc0d227204fa4815442a59a424c94fc6
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-04 13:02:15 -0300

    Implementação básica classe repositório de participantes utilizando JPA

---
+++ src/main/java/com/UFJF/planejaai/repositories/ParticipanteRepository.java
@@ -0,0 +1,7 @@
+package com.UFJF.planejaai.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.UFJF.planejaai.domain.Participante;
+
+public interface ParticipanteRepository extends CrudRepository<Participante, Long> {
+}
