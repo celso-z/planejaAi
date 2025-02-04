4c8e9d4e02360f5ca105ab4fe2c0ebcd6db10ef5
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-04 13:03:36 -0300

    Implementação básica classe repositório de organizadores utilizando JPA

---
+++ src/main/java/com/UFJF/planejaai/repositories/OrganizadorRepository.java
@@ -0,0 +1,8 @@
+package com.UFJF.planejaai.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.UFJF.planejaai.domain.Organizador;
+
+public interface OrganizadorRepository extends CrudRepository<Organizador, Long> {
+
+}
