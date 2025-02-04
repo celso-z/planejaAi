3721b53577cea8be5861465057f824b091d50e8f
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-04 12:53:45 -0300

    Implementação testes classe Inscricao

---
+++ src/test/java/com/UFJF/planejaai/domain/InscricaoTests.java
@@ -0,0 +1,81 @@
+package com.UFJF.planejaai.domain;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InscricaoTests {
+	
+	@Test
+	void setConfirmadaTest() {
+		Inscricao example = new Inscricao();
+		
+		Assertions.assertFalse(example.getConfirmada(), "Inscrição deve ser automaticamente inicializada para false");
+		example.setConfirmada(true);
+		Assertions.assertTrue(example.getConfirmada(), "Método setter do campo confirmação de inscrição não funciona!");
+		example.setConfirmada(false);
+		Assertions.assertTrue(example.getConfirmada(), "Classe Inscrição não deve permitir desconfirmar inscrições confirmadas.");
+	}
+	
+	@Test
+	void toStringTest() {
+		Inscricao example = new Inscricao();
+		
+		try{
+			example.toString();
+			Assertions.fail("Classe inscrição permitindo uso de campos não propriamente inicializados");
+		}catch(IllegalStateException e){
+			
+		}
+	}
+	
+	@Test
+	void endpointTest() {
+		Inscricao example = new Inscricao();
+		
+		try {
+			example.generateEndpointCode();
+			Assertions.fail();
+		}catch(IllegalStateException e){
+			
+		}catch(Exception e) {
+			Assertions.fail("Implemente ou altere função de hashing");
+		}
+		
+		example.setConfirmada(true);
+		example.setId(Long.valueOf(0));
+		example.setParticipante(new Participante());
+		example.setAtividade(new Atividade());
+		
+		try {
+			example.generateEndpointCode();
+			String endpoint = example.getEndpointCode();
+			
+			if(endpoint == null || endpoint == "") {
+				Assertions.fail("Função geradora de endereço de endpoints não funciona!");
+			}
+			if(endpoint.length() != 40) {
+				Assertions.fail("Função geradora de endereço de enpoints fora de especificação: " +
+						"tamanho diferente de 40.");
+			}
+		}catch(Exception e) {
+			Assertions.fail("Função geradora de endereço de endpoints não funciona!");
+		}
+		
+		Inscricao example2 = new Inscricao();
+		example2.setConfirmada(false);
+		example2.setId(Long.valueOf(1));
+		example2.setParticipante(new Participante());
+		example2.setAtividade(new Atividade());
+		try {
+			example2.generateEndpointCode();
+			Assertions.assertNotEquals(example.getEndpointCode(), example2.getEndpointCode(), "Função geradora de endpoints é pura");
+		}catch(Exception e) {
+			Assertions.fail("Função geradora de endereço de endpoints não funciona!");
+		}
+	}
+	
+	
+}
