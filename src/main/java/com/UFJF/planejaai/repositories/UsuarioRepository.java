1505a59aa9008076ec4258fcfac4b5c99b424830
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-06 13:31:33 -0300

    Adicionada pesquisa de usuários por e-mail no repositório para validação de usuários

--- src/main/java/com/UFJF/planejaai/repositories/UsuarioRepository.java
+++ src/main/java/com/UFJF/planejaai/repositories/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.UFJF.planejaai.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.UFJF.planejaai.domain.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+	Optional<Usuario> findByEmail(String email);
 }
