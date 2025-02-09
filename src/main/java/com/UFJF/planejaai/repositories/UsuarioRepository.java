b07947042d04feab39add84d548eeb798a08c5c3
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-02-09 17:01:39 -0300

    Adicionado ao repositório método para busca por tipo de usuário, para maior facilidade na implementação de todos os tipos de usuário na classe UsuarioService

--- src/main/java/com/UFJF/planejaai/repositories/UsuarioRepository.java
+++ src/main/java/com/UFJF/planejaai/repositories/UsuarioRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.UFJF.planejaai.domain.Usuario;
+import com.UFJF.planejaai.domain.TipoUsuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
 	Optional<Usuario> findByEmail(String email);
+	Iterable<Usuario> findAllByTipoUsuario(TipoUsuario tipo);
 }
