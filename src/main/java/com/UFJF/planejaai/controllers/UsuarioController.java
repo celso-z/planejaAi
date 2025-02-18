2335698afd0c84ff6295c5a54e482dd137b6a7ce
Lauriely Lourenço <laurielylourenco@gmail.com>
2025-02-17 23:49:13 -0300

    Refatoração de utilizar parametro na endpoint do usuario

--- src/main/java/com/UFJF/planejaai/controllers/UsuarioController.java
+++ src/main/java/com/UFJF/planejaai/controllers/UsuarioController.java
@@ -1,7 +1,7 @@
 package com.UFJF.planejaai.controllers;
 
 import java.util.List;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,86 +25,95 @@ import com.UFJF.planejaai.services.UsuarioService;
 public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@PostMapping("/organizador")
-	public ResponseEntity<?> criarOrganizador(@RequestBody Organizador organizador){
-		try{
+	public ResponseEntity<?> criarOrganizador(@RequestBody Organizador organizador) {
+		try {
 			usuarioService.criarUsuario(organizador);
-		}catch(IllegalArgumentException ex){
+		} catch (IllegalArgumentException ex) {
 			return ResponseEntity.badRequest().build();
 		}
 		return ResponseEntity.ok().build();
 	}
+
 	@PostMapping("/palestrante")
-	public ResponseEntity<?> criarPalestrante(@RequestBody Palestrante palestrante){
-		try{
+	public ResponseEntity<?> criarPalestrante(@RequestBody Palestrante palestrante) {
+		try {
 			usuarioService.criarUsuario(palestrante);
-		}catch(IllegalArgumentException ex){
+		} catch (IllegalArgumentException ex) {
 			return ResponseEntity.badRequest().build();
 		}
 		return ResponseEntity.ok().build();
 	}
+
 	@PostMapping("/participante")
-	public ResponseEntity<?> criarParticipante(@RequestBody Participante participante){
-		try{
+	public ResponseEntity<?> criarParticipante(@RequestBody Participante participante) {
+		try {
 			usuarioService.criarUsuario(participante);
-		}catch(IllegalArgumentException ex){
+		} catch (IllegalArgumentException ex) {
 			return ResponseEntity.badRequest().build();
 		}
 		return ResponseEntity.ok().build();
 	}
+
 	@GetMapping("/usuarios")
-	public List<UsuarioDTO> getUsuarios(){
+	public List<UsuarioDTO> getUsuarios() {
 		return usuarioService.getAllUsuarios();
 	}
-	
+
 	@GetMapping("/usuario")
-	public ResponseEntity<?> getUsuario(@RequestBody String email){
+	public ResponseEntity<?> getUsuario(@RequestParam String email) {
 		try {
 			UsuarioDTO dto = usuarioService.getUsuario(email);
-			return ResponseEntity.ok(dto);
-		}catch(UsernameNotFoundException ex) {
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			if (dto != null) {
+				return ResponseEntity.ok(dto);
+			} else {
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			}
+		} catch (UsernameNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+		} catch (Exception ex) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro interno");
 		}
 	}
-	
+
 	@GetMapping("/organizador")
-	public List<UsuarioDTO> getOrganizadores(){
+	public List<UsuarioDTO> getOrganizadores() {
 		return usuarioService.getAllUsuariosPorTipo(TipoUsuario.ORGANIZADOR);
 	}
-	
+
 	@GetMapping("/palestrante")
-	public List<UsuarioDTO> getPalestrante(){
+	public List<UsuarioDTO> getPalestrante() {
 		return usuarioService.getAllUsuariosPorTipo(TipoUsuario.PALESTRANTE);
 	}
-	
+
 	@GetMapping("/participante")
-	public List<UsuarioDTO> getParticipante(){
+	public List<UsuarioDTO> getParticipante() {
 		return usuarioService.getAllUsuariosPorTipo(TipoUsuario.PARTICIPANTE);
 	}
-	
+
 	@DeleteMapping("/usuario")
-	public ResponseEntity<?> deleteUsuario(@RequestBody String email){
+	public ResponseEntity<?> deleteUsuario(@RequestBody String email) {
 		try {
 			usuarioService.deleteByEmail(email);
-		}catch(UsernameNotFoundException ex) {}
-		catch(Exception e) {
-			//Caso encontre usuario mas não consiga deletar por erro de banco
+		} catch (UsernameNotFoundException ex) {
+		} catch (Exception e) {
+			// Caso encontre usuario mas não consiga deletar por erro de banco
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
-	
+
 	@PutMapping("/organizador")
-	public ResponseEntity<?> atualizarUsuario(@RequestBody Organizador organizador){
-		try{
+	public ResponseEntity<?> atualizarUsuario(@RequestBody Organizador organizador) {
+		try {
 			usuarioService.atualizarUsuario(organizador);
-		}catch(IllegalArgumentException ex){
+		} catch (IllegalArgumentException ex) {
 			return ResponseEntity.badRequest().build();
-		}catch(UsernameNotFoundException ex){
+		} catch (UsernameNotFoundException ex) {
 			return ResponseEntity.badRequest().build();
 		}
 		return ResponseEntity.ok().build();
 	}
-	
+
 }
